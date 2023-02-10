7a984744a1eb7ee5d45da4e26cef37083b0c5879
natalia <dnataliacr@gmail.com>
2023-02-10 11:45:49 -0300

    handle routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Route, Routes } from 'react-router-dom';
+import SearchBar from './components/SearchBar';
+import Home from './components/screens/Home';
+import ProductList from './components/screens/ProductList';
+import SingleProduct from './components/screens/SingleProduct';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar />
+
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path={`/items`} element={<ProductList />} />
+          <Route path='/items/:id' element={<SingleProduct />} />
+          <Route path='*' element={<h1> Not found</h1>} />
+        </Routes>
+      </main>
     </div>
   );
 }
