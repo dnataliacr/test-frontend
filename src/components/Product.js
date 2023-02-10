63f3456c2cc135aa1e07aeb211873a5ead445789
natalia <dnataliacr@gmail.com>
2023-02-10 12:31:39 -0300

    product template

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,4 +3,20 @@ import { useNavigate } from 'react-router-dom';
 
 const Product = ({ product }) => {
 
+    return (<div className='product-container'>
+            <div
+                className="product-picture">
+                <img src={product?.picture} alt="product image" />
+            </div>
+            <div className="product-content">
+                <span> $ {product?.price?.amount}</span>
+                <h2>{product?.title}</h2>
+            </div>
+        </div>
+        <div className="product-location">
+            <span>{product?.location}</span>
+        </div>
+
+    </div>)
+
 export default Product
