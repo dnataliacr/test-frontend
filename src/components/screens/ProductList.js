1eb6f4a9c00ae5a6a63d4463005fd1c351d03ff4
natalia <dnataliacr@gmail.com>
2023-02-10 12:28:37 -0300

    fetch data

--- src/components/screens/ProductList.js
+++ src/components/screens/ProductList.js
@@ -7,6 +7,18 @@ const ProductList = ({ }) => {
     const [category, setCategory] = useState([])
     let [searchParams, setSearchParams] = useSearchParams();
     const query = searchParams.get('search')
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get(`http://localhost:8000/api/items?q=${query}`)
+            const productsFirstItems = response?.data?.items.slice(0, 4)
+
+            setProducts(productsFirstItems)
+            setCategory(response?.data?.categories)
+        };
+
+        fetchData();
+    }, [query])
+
     return (<div className='container product-list-container'>
     </div>
     )
