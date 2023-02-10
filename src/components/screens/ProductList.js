e4b7144d4b5134d779a3530a4e174c232b06a719
natalia <dnataliacr@gmail.com>
2023-02-10 12:27:04 -0300

    product list state

--- src/components/screens/ProductList.js
+++ src/components/screens/ProductList.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import axios from "axios";
 
-const ProductList = () => {
-    return (
-        <h1>
-            ProductList
-        </h1>
+const ProductList = ({ }) => {
+    const [products, setProducts] = useState([])
+    const [category, setCategory] = useState([])
+    let [searchParams, setSearchParams] = useSearchParams();
+    const query = searchParams.get('search')
+    return (<div className='container product-list-container'>
+    </div>
     )
 }
 
