bda319afa00da57bf8da1a5d4ba04b21aafa658f
natalia <dnataliacr@gmail.com>
2023-02-10 12:11:46 -0300

    search bar state & route handlers

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, Link, useSearchParams } from 'react-router-dom';
 
-const SearchBar = () => {
+    let navigate = useNavigate();
+    let [searchParams, setSearchParams] = useSearchParams();
+    const query = searchParams.get('search')
+    const [q, setQ] = useState('')
+    const handleRoutes = () => {
+            navigate(`/items?search=${q}`)
+    const handleKeyPress = (event) => {
+            handleRoutes()
+    }
+    const handleClick = () => {
+            handleRoutes()
+        }
+    }
     return (
         <h1>
             SearchBar
