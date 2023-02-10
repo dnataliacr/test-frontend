2cc7fe215066f0e0b9daaca8844a37ca93a71d18
natalia <dnataliacr@gmail.com>
2023-02-10 12:02:46 -0300

    call api

--- server.js
+++ server.js
@@ -5,14 +5,31 @@ var cors = require('cors')
 const app = express()
 
 app.get('/api/items', cors(), async (req, res) => {
+    try {
+        const response = await axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${req.query.q}`)
+        res.json(response.data)
+    } catch (error) {
+        res.status(500).send(error.message)
 
-    res.json('items')
+    }
 })
 app.get('/api/items/:id', cors(), async (req, res) => {
-    res.json('item by id')
+    try {
+        const response = await axios.get(`https://api.mercadolibre.com/items/${req.params.id}`)
+        res.json(response.data)
+    } catch (error) {
+        res.status(500).send(error.message)
+
+    }
 })
 app.get('/api/items/:id/description', cors(), async (req, res) => {
-    res.json('item description')
+    try {
+        const response = await axios.get(`https://api.mercadolibre.com/items/${req.params.id}/description`)
+        res.json(response.data)
+    } catch (error) {
+        res.status(500).send(error.message)
+
+    }
 })
 
 
