690204120bb52c9010ce9780d578da2a0a61b4c6
natalia <dnataliacr@gmail.com>
2023-02-10 12:38:57 -0300

    single product template

--- src/components/screens/SingleProduct.js
+++ src/components/screens/SingleProduct.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react"
+import { useParams } from 'react-router-dom';
+import axios from "axios";
+import BreadCrumb from "../Breadcrumb";
 
-const SingleProduct = () => {
+const SingleProduct = ({ product }) => {
+    let { id } = useParams();
+
     return (
-        <h1>
-            SingleProduct
-        </h1>
+        <div className="container">
+            <BreadCrumb categories={categories} />
+            <div className="single-product-container">
+                <section>
+                    <div className="product-image">
+                        <img src={singleProduct?.picture} />
+                    </div>
+                    <div className="product-description">
+                        <h3>Descripción del producto</h3>
+                        <p>{description}</p>
+                    </div>
+                </section>
+                <aside>
+                    <div className="product-data">
+                        <h1> {singleProduct?.title}</h1>
+                        <span>  ${singleProduct?.price?.amount}</span>
+                        <Button label={'Comprar'} type={'primary'} url={''} />
+                    </div>
+                </aside>
+
+
+
+            </div>
+        </div>
     )
 }
 
